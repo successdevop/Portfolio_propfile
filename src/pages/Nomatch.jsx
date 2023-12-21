0a182d3edfcdaa35f5902ecd0e2ab64dc1627b57
successdevop <successraphael28@gmail.com>
2023-12-21 20:18:37 +0100

    made some changes to the nomatch page

--- src/pages/Nomatch.jsx
+++ src/pages/Nomatch.jsx
@@ -4,6 +4,7 @@ function Nomatch() {
   return (
     <div className="border-t-[1rem] border-t-[#f1f5f8] bg-[#fffaeb] px-8 py-[20%] text-center text-[2rem] capitalize text-[#e9b949]">
       No route match this part
+      <br></br>
       <Link
         to="/"
         className="mt-[5rem] inline-block rounded-3xl bg-[#e9b949] px-6 py-3 text-[#102a42]"
